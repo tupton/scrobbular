09cb50fefbc8e0a9cb5c5a9e5007fb48703ef921
tupton <thomasupton@gmail.com>
2010-12-18 11:30:11 -0500

    Cleanup and documentation.

--- pylastfm.py
+++ pylastfm.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+
 import argparse
 import ConfigParser
 import hashlib
@@ -12,23 +13,24 @@ import webbrowser
 import xml.dom.minidom
 
 class LastfmSessionDb(object):
-    """An interface to the lastfm session database"""
+    """An interface to the Last.fm session database"""
     def __init__(self):
         self.conn = None
 
-    def get_connection(self):
+    def _get_connection(self):
         if (self.conn is None):
             self.conn = sqlite3.connect('/usr/local/share/pylastfm/sqlite3/session.db')
 
         return self.conn
 
-    def get_cursor(self):
+    def _get_cursor(self):
         conn = self.get_connection()
+
         return conn.cursor()
 
     def get_session_key(self, username):
         """Fetch the session key from the db"""
-        c = self.get_cursor()
+        c = self._get_cursor()
         params = (username.lower(), )
         c.execute("select session from lastfm_session where username = ? limit 1", params)
 
@@ -43,7 +45,7 @@ class LastfmSessionDb(object):
 
     def set_session_key(self, username, session_key):
         """Put the session key in the db"""
-        conn = self.get_connection()
+        conn = self._get_connection()
         c = conn.cursor()
         params = (username.lower(), session_key, )
         c.execute("insert or replace into lastfm_session values (?, ?)", params)
@@ -51,7 +53,7 @@ class LastfmSessionDb(object):
         c.close()
 
 class LastfmApi(object):
-    """An interface to the Lastfm API"""
+    """An interface to the Last.fm API"""
 
     API_URL = "http://ws.audioscrobbler.com/2.0/"
     API_KEY = "24ff1477a4127b410d1f0b660c3049ee"
@@ -62,24 +64,31 @@ class LastfmApi(object):
         self.username = username
 
     def _add_api_signature_to_params(self, params):
-        """docstring for _add_api_signature_to_params"""
+        """Calculate and add the API signature to the given parameters"""
+
         api_sig = self._get_api_signature(params)
         params['api_sig'] = api_sig
         return params
 
     def _build_auth_url(self, params, post=False):
+        """Build a URL to send to the authorization end point"""
+
         return self._build_url(self.AUTH_URL, params, post)
 
     def _build_request_url(self, params, post=False):
+        """Build a URL to send to the API end point"""
+
         return self._build_url(self.API_URL, params, post)
 
     def _build_param_string(self, params):
         """Build a param string from the given dictionary of params"""
+
         encoded = urllib.urlencode([(k, v) for k,v in params.iteritems()])
         return encoded
 
     def _build_url(self, base, params, post=False):
-        """Build a request URL"""
+        """Build a request URL into an object that can be passed to urllib2.urlopen"""
+
         param_string = self._build_param_string(params)
 
         if post is True:
@@ -90,7 +99,8 @@ class LastfmApi(object):
         return url
 
     def _get_api_signature(self, params):
-        """get the api signature for the given parameters"""
+        """Get the API signature for the given parameters"""
+
         keys = params.keys()
         keys.sort()
         sort = [(k, params.get(k)) for k in keys]
@@ -102,6 +112,7 @@ class LastfmApi(object):
 
     def _handle_session_key_response(self, response):
         """Get a session key from a response"""
+
         dom = xml.dom.minidom.parseString(response)
         lfm = dom.getElementsByTagName('lfm')[0]
         if lfm.getAttribute('status').lower() == 'ok':
@@ -115,6 +126,7 @@ class LastfmApi(object):
 
     def _handle_token_response(self, response):
         """Get a token from the given response"""
+
         dom = xml.dom.minidom.parseString(response)
         lfm = dom.getElementsByTagName('lfm')[0]
         if lfm.getAttribute('status').lower() == 'ok':
@@ -126,6 +138,8 @@ class LastfmApi(object):
             raise Exception('Bad response from Last.fm')
 
     def _get_xml_text(self, nodelist):
+        """Get the text content of a given set of nodes (.childNodes of the element whose text you want)"""
+
         rc = []
         for node in nodelist:
             if node.nodeType == node.TEXT_NODE:
@@ -134,12 +148,14 @@ class LastfmApi(object):
 
     def _get_request_token(self):
         """Get a Last.fm request token"""
+
         params = dict({'method': 'auth.gettoken', 'api_key': self.API_KEY})
         response = self._send_request(self._build_request_url(params))
         return self._handle_token_response(response)
 
     def _get_session_key(self):
         """Get the session key for a user"""
+
         username = self.username
         db = LastfmSessionDb()
         session_key = db.get_session_key(username)
@@ -158,12 +174,15 @@ class LastfmApi(object):
         return session_key
 
     def _send_request(self, url):
+        """Send a request and return the raw response"""
+
         response = urllib2.urlopen(url)
         text = response.read()
         return text
 
     def scrobble(self, artist, track, duration):
-        """Submit a track to lastfm for the given username and password"""
+        """Submit a track to Last.fm for the given username and password"""
+
         session_key = self._get_session_key()
         params = self._add_api_signature_to_params(dict({'method': 'track.scrobble',
             'track': track, 'artist': artist, 'duration': duration,
@@ -172,7 +191,8 @@ class LastfmApi(object):
         self._send_request(self._build_request_url(params, post=True))
 
     def update_now_playing(self, artist, track, duration):
-        """Submit a track to lastfm to update the now playing status"""
+        """Submit a track to Last.fm to update the now playing status"""
+
         session_key = self._get_session_key()
         params = self._add_api_signature_to_params(dict({'method': 'track.updatenowplaying',
             'track': track, 'artist': artist,
@@ -180,7 +200,7 @@ class LastfmApi(object):
         self._send_request(self._build_request_url(params, post=True))
 
 def create_arg_parser():
-    """Create an option parser for last.fm-specific options"""
+    """Create an option parser for Last.fm-specific options"""
 
     parser = argparse.ArgumentParser(description="Submit scrobbles and now playing updates to Last.fm")
 

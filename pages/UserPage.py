714bb19802b52e2ae8f0bb45f18575f7803426c6
tupton <thomasupton@gmail.com>
2010-12-20 20:20:00 -0500

    Extract getting the user name and session key to make it easier to maintain.

--- pages/UserPage.py
+++ pages/UserPage.py
@@ -6,16 +6,23 @@ from google.appengine.ext.webapp import template
 
 class UserPage(webapp.RequestHandler):
     def get(self):
+        user = self.get_username_from_path()
+        if user:
+            self.render_user_page(user)
+        else:
+            self.redirect('/')
+
+    def get_username_from_path(self):
+        """Get the username from the path that was used to request this page"""
+        user = None
         path = self.request.path
         if len(path) > len('/user/'):
             user = path[len('/user/'):].split('/')[0] 
-            session_key = None
-            if user:
-                session_key = scrobble.get_session_key(user)
-            self.response.out.write(template.render('templates/user.html', {'username': user,
+        return user
+
+    def render_user_page(self, user):
+        """Render the user page for the given user"""
+        session_key = scrobble.get_session_key(user)
+        self.response.out.write(template.render('templates/user.html', {'username': user,
                                                                             'session_key': session_key}))
-        else:
-            self.response.out.write("<br/>".join(['url: ' + self.request.url,
-                                                'path: ' + self.request.path,
-                                                'query_string: ' + self.request.query_string]))
 

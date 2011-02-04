8b37dc9ae128d52f833152762a8dd6cd28a89654
tupton <thomasupton@gmail.com>
2011-02-04 14:37:20 -0600

    Revert "Revert "Add a date_modified column to the LastfmSession db model.""
    This reverts commit 8506cf8e6c4fb960d318a144187343aeff352741.

--- scrobble/LastfmSession.py
+++ scrobble/LastfmSession.py
@@ -11,6 +11,7 @@ class LastfmSession(db.Model):
     session_key = db.StringProperty(required=True)
     secret = db.StringProperty(required=True)
     date_created = db.DateTimeProperty(auto_now_add=True)
+    date_modified = db.DateTimeProperty(auto_now=True)
 
 def _get_session(user):
     """Get the session for a user"""

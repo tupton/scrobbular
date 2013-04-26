05ee99fd79a90644c09fea1e394181cfe584210c
Thomas Upton <thomasupton@gmail.com>
2013-04-26 14:08:33 -0500

    Convert to high-availability store and python 2.7.

--- hello.py
+++ hello.py
@@ -1,21 +1,14 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-
+import webapp2
 import pages
 
-application = webapp.WSGIApplication(
-                                     [('/', pages.IndexPage),
-                                      ('/auth', pages.AuthPage),
-                                      ('/howto', pages.HowtoPage),
-                                      ('/now_playing', pages.NowPlayingPage),
-                                      ('/user', pages.UserPage),
-                                      ('/scrobble', pages.ScrobblePage),
-                                      ('/user/secret', pages.ChangeSecretPage),
-                                      ('/user/delete', pages.DeleteUserPage)],
-                                     debug=False)
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
+app = webapp2.WSGIApplication(
+    [('/', pages.IndexPage),
+    ('/auth', pages.AuthPage),
+    ('/howto', pages.HowtoPage),
+    ('/now_playing', pages.NowPlayingPage),
+    ('/user', pages.UserPage),
+    ('/scrobble', pages.ScrobblePage),
+    ('/user/secret', pages.ChangeSecretPage),
+    ('/user/delete', pages.DeleteUserPage)],
+    debug=False
+)
